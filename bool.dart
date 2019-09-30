1ece0b7c1f11f12814507532111c75c7cb1bd912
lohanisuraj <suraz191@gmail.com>
2019-09-30 21:23:46 +0545

    bool is done

---
+++ bool.dart
@@ -0,0 +1,13 @@
+void main(){
+  bool test;
+  test = 3 > 1;
+  print(test);
+  var str = test;
+  if(str){
+    print("condition  is true ");
+  }
+  else{
+    print("condition is false");
+  }
+  
+}
