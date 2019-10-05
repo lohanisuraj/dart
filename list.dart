68b1063f15d4abf47892275195a4396c72ba409f
lohanisuraj <suraz191@gmail.com>
2019-10-05 07:10:27 +0545

    list done

---
+++ list.dart
@@ -0,0 +1,16 @@
+void main(){
+  var lst = new List(5);
+  lst[0]=2;
+  lst[1]= "suraj";
+  lst[2]='f';
+  var lst1 = new List(1);
+  lst1[0]=1;
+  print(lst);
+  print(lst.first);
+  print(lst.isEmpty);
+  print(lst.isNotEmpty);
+  print(lst.reversed);
+  print(lst1.single);
+
+ 
+}
