5c8b13ccc5c7d432a4d7134e56425059bfd49b36
lohanisuraj <suraz191@gmail.com>
2019-10-21 20:23:22 +0545

    greater number

---
+++ greatestNumber.dart
@@ -0,0 +1,10 @@
+void main(){
+  int a = 5,b = 10, c = 2;
+  if(a>b && a>c){
+    print("$a is greater");
+  }else if(b>a && b>c){
+    print("$b is greater");
+  }else{
+    print("$c is greater");
+  }
+}
